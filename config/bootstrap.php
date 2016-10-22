9ee4ca7aa6f80a2c980e65d7e8d4f83b26e1cfa6
Narendra Vaghela <narendravaghela4389@gmail.com>
2016-10-22 15:48:23 +0530

    Update request and response handling

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,14 +6,9 @@
 
 use Cake\Core\Configure;
 use Cake\Event\EventManager;
-use RestApi\Event\ApiRequestHandler;
 
 // Set custom exception renderer
 Configure::write('Error.exceptionRenderer', 'RestApi\Error\AppExceptionRenderer');
-
-// Register listner
-EventManager::instance()->on(new ApiRequestHandler());
-
 
 /*
  * Read configuration file and inject configuration
--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,6 +3,8 @@
 namespace RestApi\Controller;
 
 use Cake\Controller\Controller;
+use Cake\Core\Configure;
+use Cake\Event\Event;
 
 /**
  * Application Controller
@@ -42,5 +44,47 @@ class AppController extends Controller
         parent::initialize();
 
         $this->loadComponent('RequestHandler');
+        $this->_buildResponse();
+    }
+
+    public function beforeFilter(Event $event)
+    {
+        parent::beforeFilter($event);
+
+        if ('OPTIONS' === $this->request->method()) {
+            $this->response->statusCode(200);
+
+            return $this->response;
+        }
+    }
+
+    public function afterFilter(Event $event)
+    {
+        // TODO: log request
+        parent::afterFilter($event);
+    }
+
+    /**
+     * Prepares the response object with content type and cors headers.
+     *
+     * @param Event $event The event object either beforeDispatch or afterDispatch
+     *
+     * @return bool true
+     */
+    private function _buildResponse()
+    {
+        $this->response->type('json');
+
+        if (Configure::read('ApiRequest.cors.enabled')) {
+            $this->response->cors($this->request)
+                ->allowOrigin(Configure::read('ApiRequest.cors.origin'))
+                ->allowMethods(['GET', 'POST', 'OPTIONS'])
+                ->allowHeaders(['Content-Type, Authorization, Accept, Origin'])
+                ->allowCredentials()
+                ->maxAge(2628000)
+                ->build();
+        }
+
+        return true;
     }
 }
