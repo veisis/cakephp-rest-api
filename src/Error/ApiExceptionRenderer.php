289b44c89d0a0bd93fc48590e13cdabde9786b74
Narendra Vaghela <narendravaghela4389@gmail.com>
2016-10-24 18:23:42 +0530

    Update exception message rendering

--- src/Error/ApiExceptionRenderer.php
+++ src/Error/ApiExceptionRenderer.php
@@ -2,11 +2,13 @@
 
 namespace RestApi\Error;
 
-use Cake\Core\Configure;
 use Cake\Error\ExceptionRenderer;
 use Cake\Network\Response;
 use Exception;
 use RestApi\Controller\ApiErrorController;
+use RestApi\Routing\Exception\InvalidTokenException;
+use RestApi\Routing\Exception\InvalidTokenFormatException;
+use RestApi\Routing\Exception\MissingTokenException;
 
 /**
  * API Exception Renderer.
@@ -24,6 +26,42 @@ class ApiExceptionRenderer extends ExceptionRenderer
     protected function _getController()
     {
         return new ApiErrorController();
+    }
+
+    /**
+     * Handles MissingTokenException.
+     *
+     * @param MissingTokenException $exception MissingTokenException
+     *
+     * @return type
+     */
+    public function missingToken($exception)
+    {
+        return $this->__prepareResponse($exception, ['customMessage' => true]);
+    }
+
+    /**
+     * Handles InvalidTokenFormatException.
+     *
+     * @param InvalidTokenFormatException $exception InvalidTokenFormatException
+     *
+     * @return Response
+     */
+    public function invalidTokenFormat($exception)
+    {
+        return $this->__prepareResponse($exception, ['customMessage' => true]);
+    }
+
+    /**
+     * Handles InvalidTokenException.
+     *
+     * @param InvalidTokenException $exception InvalidTokenException
+     *
+     * @return Response
+     */
+    public function invalidToken($exception)
+    {
+        return $this->__prepareResponse($exception, ['customMessage' => true]);
     }
 
     /**
@@ -47,12 +85,12 @@ class ApiExceptionRenderer extends ExceptionRenderer
             ],
         ];
 
-        Configure::write('exceptionMessage', $exception->getMessage());
+        if (isset($options['customMessage']) && $options['customMessage']) {
+            $body['result']['error'] = $exception->getMessage();
+        }
 
         $response->type('json');
         $response->body(json_encode($body));
-
-        ApiRequestLogger::log($this->_getController()->request, $response);
 
         return $response;
     }
