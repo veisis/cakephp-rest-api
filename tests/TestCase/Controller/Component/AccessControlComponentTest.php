6eb43151cb6e127f9b8e4c6fea148ab83762b8e5
Narendra Vaghela <narendravaghela4389@gmail.com>
2016-12-14 16:14:32 +0530

    New tests

--- tests/TestCase/Controller/Component/AccessControlComponentTest.php
+++ tests/TestCase/Controller/Component/AccessControlComponentTest.php
@@ -239,6 +239,64 @@ class AccessControlComponentTest extends TestCase
         unset($Component);
     }
 
+    public function testInvalidTokenFormat()
+    {
+        $config = [
+            'jwtAuth' => [
+                'enabled' => true,
+                'cypherKey' => 'R1a#2%dY2fX@3g8r5&s4Kf6*sd(5dHs!5gD4s',
+                'tokenAlgorithm' => 'HS256'
+            ]
+        ];
+        Configure::write('ApiRequest', $config);
+
+        $this->setExpectedException('RestApi\Routing\Exception\InvalidTokenFormatException');
+
+        $requestConfig = [
+            'params' => ['allowWithoutToken' => false]
+        ];
+        $request = new Request($requestConfig);
+        $request->env('HTTP_AUTHORIZATION', "NotBearer jwt.token");
+
+        $Controller = new \TestApp\Controller\FooController($request);
+        $registry = new ComponentRegistry($Controller);
+        $Component = new AccessControlComponent($registry);
+        $event = new Event('Controller.startup', $Controller);
+        $Component->startup($event);
+
+        unset($Controller);
+        unset($Component);
+    }
+
+    public function testInvalidToken()
+    {
+        $config = [
+            'jwtAuth' => [
+                'enabled' => true,
+                'cypherKey' => 'R1a#2%dY2fX@3g8r5&s4Kf6*sd(5dHs!5gD4s',
+                'tokenAlgorithm' => 'HS256'
+            ]
+        ];
+        Configure::write('ApiRequest', $config);
+
+        $this->setExpectedException('RestApi\Routing\Exception\InvalidTokenException');
+
+        $requestConfig = [
+            'params' => ['allowWithoutToken' => false]
+        ];
+        $request = new Request($requestConfig);
+        $request->env('HTTP_AUTHORIZATION', "Bearer jwt.token");
+
+        $Controller = new \TestApp\Controller\FooController($request);
+        $registry = new ComponentRegistry($Controller);
+        $Component = new AccessControlComponent($registry);
+        $event = new Event('Controller.startup', $Controller);
+        $Component->startup($event);
+
+        unset($Controller);
+        unset($Component);
+    }
+
     public function testMissingAccessToken()
     {
         $config = [
