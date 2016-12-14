db800f789d7b44e6317e7c0e351b226d2238d586
Narendra Vaghela <narendravaghela4389@gmail.com>
2016-12-14 13:38:48 +0530

    Changes in tests

--- tests/TestCase/Controller/ApiControllerTest.php
+++ tests/TestCase/Controller/ApiControllerTest.php
@@ -5,8 +5,6 @@ namespace RestApi\Test\TestCase\Controller;
 use Cake\Event\Event;
 use Cake\Network\Request;
 use Cake\Network\Response;
-use Cake\Routing\RouteBuilder;
-use Cake\Routing\Router;
 use Cake\TestSuite\IntegrationTestCase;
 
 /**
@@ -57,17 +55,5 @@ class ApiControllerTest extends IntegrationTestCase
         $this->assertNotEmpty($this->controller->httpStatusCode);
         $this->assertNotEmpty($this->controller->responseStatus);
         $this->assertContains($this->controller->responseStatus, ['OK', 'NOK']);
-    }
-
-    public function testFooAction()
-    {
-        /* it should call foo/public-action endpoint from TestApp/FooController
-         * and match the resulting json response
-         */
-        $request = new Request('/foo/bar');
-        $response = new Response();
-        $Controller = new \TestApp\Controller\FooController($request, $response);
-        $Controller->render();
-        debug($Controller->response->body());
     }
 }
